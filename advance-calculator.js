0fab9bbabdc0d3cd9390d9a2bcad59109c0c2341
shanti96 <106430039+shanti96@users.noreply.github.com>
2023-11-08 15:00:31 +0530

    Add files via upload

---
+++ advance-calculator.js
@@ -0,0 +1,43 @@
+function on1()
+{
+    document.getElementById('result').disabled=false;
+    document.getElementById('1').disabled=false;
+    document.getElementById('2').disabled=false;
+    document.getElementById('3').disabled=false;
+    document.getElementById('4').disabled=false;
+    document.getElementById('5').disabled=false;
+    document.getElementById('6').disabled=false;
+    document.getElementById('7').disabled=false;
+    document.getElementById('8').disabled=false;
+    document.getElementById('9').disabled=false;
+    document.getElementById('0').disabled=false;
+    document.getElementById('+').disabled=false;
+    document.getElementById('-').disabled=false;
+    document.getElementById('*').disabled=false;
+    document.getElementById('/').disabled=false;
+    document.getElementById('%').disabled=false;
+    document.getElementById('=').disabled=false;
+    document.getElementById('clear').disabled=false;
+} 
+
+function off2()
+{
+    document.getElementById('result').disabled=true;
+    document.getElementById('1').disabled=true;
+    document.getElementById('2').disabled=true;
+    document.getElementById('3').disabled=true;
+    document.getElementById('4').disabled=true;
+    document.getElementById('5').disabled=true;
+    document.getElementById('6').disabled=true;
+    document.getElementById('7').disabled=true;
+    document.getElementById('8').disabled=true;
+    document.getElementById('9').disabled=true;
+    document.getElementById('0').disabled=true;
+    document.getElementById('+').disabled=true;
+    document.getElementById('-').disabled=true;
+    document.getElementById('*').disabled=true;
+    document.getElementById('/').disabled=true;
+    document.getElementById('%').disabled=true;
+    document.getElementById('=').disabled=true;
+    document.getElementById('clear').disabled=true;
+} 
